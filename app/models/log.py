b992013b3c684b4d57c903204b6f4ac9b35407bc
adamba-cloud <philipcraig364@gmail.com>
2026-05-07 07:46:14 +0300

    Add function to create log table in SQLite

--- app/models/log.py
+++ app/models/log.py
@@ -1,1 +1,17 @@
+import sqlite3
 
+def create_log_table(db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS logs(
+        id INTEGER PRIMARY KEY,
+        action TEXT,
+        user TEXT,
+        time TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
