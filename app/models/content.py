1e7cc00778eeb989969242cbc20764bb6f16358b
adamba-cloud <philipcraig364@gmail.com>
2026-05-07 07:45:37 +0300

    Update content.py

--- app/models/content.py
+++ app/models/content.py
@@ -1,1 +1,17 @@
+import sqlite3
 
+def create_content_table(db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS content(
+        id INTEGER PRIMARY KEY,
+        type TEXT,
+        link TEXT,
+        title TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
