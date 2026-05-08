a423316896be03006b50b481bb64d6855aee8400
adamba-cloud <philipcraig364@gmail.com>
2026-05-08 11:55:10 +0300

    Update run.py

--- run.py
+++ run.py
@@ -29,6 +29,7 @@ with app.app_context():
 from app.routes.landing import landing_bp
 from app.routes.auth import auth_bp
 from app.routes.user import user_bp
+from app.routes.admin import admin_bp   # 🔥 ADDED
 
 # =========================
 # REGISTER BLUEPRINTS
@@ -36,6 +37,7 @@ from app.routes.user import user_bp
 app.register_blueprint(landing_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(user_bp)
+app.register_blueprint(admin_bp)        # 🔥 ADDED
 
 # =========================
 # DEBUG ROUTES
