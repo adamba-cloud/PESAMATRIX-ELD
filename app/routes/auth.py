a96f721b0ed74a9c96e544a435ed8a3840f92c84
adamba-cloud <philipcraig364@gmail.com>
2026-05-08 07:01:56 +0300

    Update auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,17 +1,9 @@
-from flask import Blueprint, request, session, redirect, current_app
-from app.services.auth_service import create_user, authenticate
-from app.utils.ui import layout
-
-auth_bp = Blueprint("auth", __name__)
-
-# =========================
-# REGISTER
-# =========================
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
+
     if request.method == "POST":
+
         account = create_user(
-            current_app.config["DATABASE"],
             request.form["name"],
             request.form["phone"],
             request.form["email"],
@@ -45,55 +37,3 @@ def register():
 
     </div>
     """)
-
-
-# =========================
-# LOGIN
-# =========================
-@auth_bp.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        user = authenticate(
-            current_app.config["DATABASE"],
-            request.form["phone"],
-            request.form["password"]
-        )
-
-        if user:
-            session["user_id"] = user["id"]
-            session["role"] = user["role"]
-            session["account"] = user["account_number"]
-            session["phone"] = user["phone"]
-
-            return redirect("/dashboard")
-
-        return layout("""
-        <div class="card" style="color:red">
-            <h3>Invalid login</h3>
-            <a href="/login" style="color:#38bdf8">Try again</a>
-        </div>
-        """)
-
-    return layout("""
-    <div class="card">
-
-        <h2 style="color:#38bdf8">Login</h2>
-
-        <form method="POST">
-            Phone:<br><input name="phone"><br><br>
-            Password:<br><input type="password" name="password"><br><br>
-
-            <button>Login</button>
-        </form>
-
-    </div>
-    """)
-
-
-# =========================
-# LOGOUT
-# =========================
-@auth_bp.route("/logout")
-def logout():
-    session.clear()
-    return redirect("/login")
