348d5db97f166966792fd77b3256e7f92e395c53
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 12:01:46 +0300

    Create admin_required.py

---
+++ backend/middleware/admin_required.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from flask_jwt_extended import get_jwt
+from flask import jsonify
+
+
+def admin_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        claims = get_jwt()
+
+        if claims.get("role") != "admin":
+            return jsonify({"error": "Admin only"}), 403
+
+        return fn(*args, **kwargs)
+
+    return wrapper
