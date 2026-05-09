b5fa646f3345b8f5f4d2a676460de45aa0dacc8a
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 16:34:09 +0300

    Update user_model.py

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
+
 class User:
-    def __init__(self, id, name, email, role):
+    def __init__(self, id, name, email, password, role="FREE"):
         self.id = id
         self.name = name
         self.email = email
+        self.password = password
         self.role = role
+        self.created_at = datetime.utcnow()
