5128b1b956050aecac6c82e4043c473216a3f9e6
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 15:59:49 +0300

    Update auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,40 +1,57 @@
 from flask import Blueprint, request, jsonify
-
-from backend.services.auth_service import register_user, login_user
-from backend.utils.jwt_helper import generate_token
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth_bp = Blueprint("auth", __name__)
 
+# temporary fake database
+users = []
 
 @auth_bp.route("/register", methods=["POST"])
 def register():
+
     data = request.json
 
-    register_user(
-        data["name"],
-        data["email"],
-        data["password"]
-    )
+    email = data.get("email")
+    password = data.get("password")
 
-    return jsonify({"message": "User registered"})
+    # check existing
+    for user in users:
+        if user["email"] == email:
+            return jsonify({"message": "User already exists"}), 400
+
+    hashed_password = generate_password_hash(password)
+
+    users.append({
+        "email": email,
+        "password": hashed_password
+    })
+
+    return jsonify({
+        "message": "User registered successfully"
+    })
 
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
+
     data = request.json
 
-    user = login_user(data["email"], data["password"])
+    email = data.get("email")
+    password = data.get("password")
 
-    if not user:
-        return jsonify({"error": "Invalid credentials"}), 401
+    for user in users:
 
-    token = generate_token(user["id"], user["role"])
+        if user["email"] == email:
+
+            if check_password_hash(user["password"], password):
+
+                token = create_access_token(identity=email)
+
+                return jsonify({
+                    "token": token
+                })
 
     return jsonify({
-        "token": token,
-        "user": {
-            "id": user["id"],
-            "email": user["email"],
-            "role": user["role"]
-        }
-    })
+        "message": "Invalid credentials"
+    }), 401
