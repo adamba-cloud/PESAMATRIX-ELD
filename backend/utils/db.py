91a7a03a330f98b1063f3a90e0a7353e7b59b05c
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 09:20:53 +0300

    Refactor database connection handling in db.py
    Refactor get_db function to use Flask's g for connection management and add close_db function for proper cleanup.

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -1,7 +1,26 @@
 import sqlite3
-from flask import current_app
+from flask import current_app, g
 
+# =========================
+# GET DATABASE CONNECTION
+# =========================
 def get_db():
-    conn = sqlite3.connect(current_app.config["DATABASE"])
-    conn.row_factory = sqlite3.Row
-    return conn
+
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"]
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+# =========================
+# CLOSE DB CONNECTION
+# =========================
+def close_db(e=None):
+
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
