9f87f987a5fbd9710a4abc590a33b37a28066249
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 16:54:37 +0300

    Update db.py

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -1,9 +1,25 @@
 import sqlite3
+from flask import g
+import os
 
-DB_NAME = "backend/database/trading_saas.db"
+DATABASE = os.path.join(os.path.dirname(__file__), "../database/trading_saas.db")
 
 
 def get_db():
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row
-    return conn
+    db = getattr(g, "_database", None)
+
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+
+    return db
+
+
+def init_db(app):
+    with app.app_context():
+        db = get_db()
+
+        with open("backend/database/schema.sql", "r") as f:
+            db.executescript(f.read())
+
+        db.commit()
