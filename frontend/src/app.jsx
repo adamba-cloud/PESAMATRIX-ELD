25269a984e56d457e79a510d1cb78ee26fa3783b
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 18:36:48 +0300

    Update app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
+import Signals from "./pages/signals";
+import Payments from "./pages/payments";
+import Profile from "./pages/profile";
 
 function App() {
   return (
     <BrowserRouter>
 
       <Routes>
+
+        <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signals" element={<Signals />} />
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/profile" element={<Profile />} />
+
       </Routes>
 
     </BrowserRouter>
