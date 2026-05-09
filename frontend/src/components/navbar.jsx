6fa0ee4e009e1786ef82fa5872765c68ae832d28
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 18:46:31 +0300

    Create navbar.jsx

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <div className="w-full h-16 bg-[#0B0F19] border-b border-gray-800 flex items-center justify-between px-6 text-white">
+
+      <h2 className="text-cyan-400 font-bold">Trading SaaS</h2>
+
+      <div className="text-gray-400 text-sm">
+        Welcome Trader
+      </div>
+
+    </div>
+  );
+}
