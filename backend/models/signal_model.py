124038c0fdac203cee12c328826db3e60d7ed402
adamba-cloud <philipcraig364@gmail.com>
2026-05-09 12:10:33 +0300

    Create signal_model.py

---
+++ backend/models/signal_model.py
@@ -0,0 +1,6 @@
+class Signal:
+    def __init__(self, pair, entry, stop_loss, take_profit):
+        self.pair = pair
+        self.entry = entry
+        self.stop_loss = stop_loss
+        self.take_profit = take_profit
