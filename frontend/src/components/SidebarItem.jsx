a8e3e7d385707cf0ceb88cccd362696ce74657ad
adamba-cloud <philipcraig364@gmail.com>
2026-05-11 13:33:28 +0300

    Delete frontend directory

--- frontend/src/components/SidebarItem.jsx
+++
@@ -1,7 +0,0 @@
-export default function SidebarItem({ label }) {
-  return (
-    <div className="px-4 py-3 rounded-xl text-gray-300 hover:bg-gray-800 hover:text-white cursor-pointer transition">
-      {label}
-    </div>
-  );
-}
--- frontend/src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js"
-
-const supabaseUrl = "https://mfqhwwqqgubontkfatyd.supabase.co"
-
-const supabaseAnonKey = "sb_publishable_T7CX34OQYp5H_nvdjKP0-Q_vDeMPCuA"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
